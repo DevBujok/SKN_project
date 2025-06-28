eaab6c7e3455960726634414afbbbba9bea56809
Paweł Bujok <dev.bujok@gmail.com>
2025-06-28 19:12:53 +0200

    feat: code refactor, added comments

--- trained_models/model_handler.py
+++ trained_models/model_handler.py
@@ -6,22 +6,13 @@ modelsFolder = "./trained_models"
 modelsInfoURL = f'{modelsFolder}/models.json'
 
 class ModelHandler:
-    def _get_models_from_json(self):
-        with open(modelsInfoURL) as f:
-            models = json.load(f)
-        return models
 
-    def _model_exists(self, name):
-        return any(m['name'] == name for m in self._get_models_from_json())
-
-    def _delete_from_json(self, model_name):
-        models = self._get_models_from_json()
-        filtered = [m for m in models if m['name'] != model_name]
-        with open(modelsInfoURL, 'w') as f:
-            json.dump(filtered, f, indent=2)
-
-    def _add_to_the_json(self, modelName, modelPath, metrics):
-        try:
+    @staticmethod
+    def _get_models_from_json():
+        """
+        Prywatna statyczna metoda do pobierania listy modeli z jsona
+        """
+        try: #proba otwarcia pliku i obsluga bledow
             with open(modelsInfoURL, 'r') as f:
                 try:
                     models = json.load(f)
@@ -31,50 +22,92 @@ class ModelHandler:
         except FileNotFoundError:
             print("Brak pliku.")
             models = []
-        if self._model_exists(modelName):
+
+        return models
+
+    def _model_exists(self, name):
+        """
+        Prywatna metoda, która sprawdza, czy dany model istnieje
+        :param name:
+        :return: boolean
+        """
+        return any(m['name'] == name for m in self._get_models_from_json())
+
+    def _delete_from_json(self, model_name):
+        """
+        Prywatna metoda do usuwania modelu z jsona
+        :param model_name:
+        :return:
+        """
+        models = self._get_models_from_json() #pobranie listy
+        filtered = [m for m in models if m['name'] != model_name] #filtrowanie po nazwie
+        with open(modelsInfoURL, 'w') as f:
+            json.dump(filtered, f, indent=2) #nadpisanie calego pliku
+
+    def _add_to_the_json(self, modelName, modelPath, metrics):
+        """
+        Prywatna metoda do dodawania modelu do jsona
+        :param modelName:
+        :param modelPath:
+        :param metrics:
+        :return:
+        """
+        models = self._get_models_from_json()
+        if self._model_exists(modelName): #jezeli model istnieje: usuniecie starego
             print("Model istnieje. Nadpisywanie...")
             self.delete_model(modelName)
-            models = self._get_models_from_json()  # trzeba ponownie wczyta
+            models = self._get_models_from_json()  # trzeba ponownie wczytac bo jest stara lista w tym momencie
 
-        models.append({
+        models.append({ #dodanie zaktualizowanego modelu
             "name": modelName,
             "path": modelPath,
             "metrics": metrics
         })
 
-        with open(modelsInfoURL, 'w') as f:
+        with open(modelsInfoURL, 'w') as f: #wgranie do pliku nowego
             json.dump(models, f, indent=2)
         print("Zapisano do pliku.")
 
     def add_model(self, model, modelName, metrics):
+        """
+        Metoda do dodawania modelu do jsona
+        :param model:
+        :param modelName:
+        :param metrics:
+        :return:
+        """
         #dodanie do jsona
-        model_path = f"{modelsFolder}/{modelName}"
-        self._add_to_the_json(modelName, model_path, metrics)
+        model_path = f"{modelsFolder}/{modelName}" #sciezka do nowego modelu
+        self._add_to_the_json(modelName, model_path, metrics) #dodanie info do jsona
 
         #zapisz model
-        os.makedirs(model_path, exist_ok=True)
-        # joblib.dump(model, './trained_models/logistic_regression_model.pkl')
-        joblib.dump(model, f"{model_path}/{modelName}.pkl")
+        os.makedirs(model_path, exist_ok=True) #wywolanie tego zapewnia ze folder istnieje
+        joblib.dump(model, f"{model_path}/{modelName}.pkl") #dump modelu
 
     @staticmethod
     def _delete_folder(modelName):
-        path_to_delete = f"{modelsFolder}/{modelName}"
+        """
+        Prywatna statyczna metoda do usuniecia istniejacego folderu
+        :param modelName:
+        :return:
+        """
+        path_to_delete = f"{modelsFolder}/{modelName}" #sciezka do usuniecia
 
-        if path_to_delete.startswith(modelsFolder):
-            print(f"path: {path_to_delete}")
-            shutil.rmtree(path_to_delete, ignore_errors=True)
+        if path_to_delete.startswith(modelsFolder): #dodatkowe sprawdzenie czy sciezka znajduje sie w katalogu domowym,
+            # bo ona moze usuwac wszystko globalnie
+            shutil.rmtree(path_to_delete, ignore_errors=True) #usuniecie
         else:
             raise PermissionError("Ścieżka poza dozwolonym katalogiem")
 
     def delete_model(self, modelName):
-        if self._model_exists(modelName):
-            self._delete_from_json(modelName)
-            self._delete_folder(modelName)
-            #dodatkowa funkcja do sprawdzenia czy folder o takiej nazwie istnieje
-
+        """
+        Metoda do usuwania modelu
+        :param modelName:
+        :return:
+        """
+        if self._model_exists(modelName): #sprawdzenie czy model istnieje
+            self._delete_from_json(modelName) #usuniecie z jsona
+            self._delete_folder(modelName) #usuniecie folderu
         else:
             print("Model o podanej nazwie nie istnieje.")
-        #usun model
-        #zaktualizuj listę
-        #wazne: sprawdzic czy model istnieje
         pass
