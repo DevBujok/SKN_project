da7e461b80b098ee3dd1a790cfb898a23d39fc3e
Pawel Bujok <dev.bujok@gmail.com>
2025-07-23 10:29:58 +0200

    feat: new models training

--- models_training/knn.py
+++ models_training/knn.py
@@ -1,32 +1,15 @@
-import pandas as pd
-import numpy as np
 from sklearn.metrics import classification_report, roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
+
+from data_handler.get_data import EncoderEnum, get_data
 from trained_models.model_handler import ModelHandler
 from sklearn.neighbors import KNeighborsClassifier
 
-data = pd.read_csv('../data_handler/heart.csv')
+chosen_encoder = EncoderEnum.ONE_HOT_ENCODER
 
-labelEncoder = LabelEncoder()
-scaler = StandardScaler()
-columns_to_scale = []
-# potem te dane trzeba bedzie jakos odkodowac
-for i in data.columns:
-    # zmieniac tylko te ktore sa nieliczbowe
-    if not isinstance(data[i].iloc[0], (np.float64, np.int64)):
-        uniqueValues = data[i].unique() #te wartości należy zmapowac na inty
-        data[i] = labelEncoder.fit_transform(data[i])
-    else:
-        columns_to_scale.append(i)
-columns_to_scale.remove("HeartDisease")
+data, encoder = get_data(encoder=chosen_encoder, force_reload=True)
 
-#knn wrazliwe na skalowanie
-data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
-
-X = data.drop(['HeartDisease', 'RestingBP', 'RestingECG'], axis=1)
+X = data.drop(['HeartDisease'], axis=1)
 Y = data['HeartDisease']
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
@@ -38,4 +21,4 @@ metrics = classification_report(y_test, y_pred, output_dict=True)
 
 modelHandler = ModelHandler()
 
-modelHandler.add_model(model, "knn_5", metrics)
+modelHandler.add_model(model, "knn_5", metrics, chosen_encoder)
--- models_training/naive_bayes.py
+++ models_training/naive_bayes.py
@@ -5,26 +5,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+
+from data_handler.get_data import EncoderEnum, get_data
 from trained_models.model_handler import ModelHandler
 
-data = pd.read_csv('../data_handler/heart.csv')
+chosen_encoder = EncoderEnum.LABEL_ENCODER
 
-labelEncoder = LabelEncoder()
-scaler = StandardScaler()
-columns_to_scale = []
-# potem te dane trzeba bedzie jakos odkodowac
-for i in data.columns:
-    # zmieniac tylko te ktore sa nieliczbowe
-    if not isinstance(data[i].iloc[0], (np.float64, np.int64)):
-        uniqueValues = data[i].unique() #te wartości należy zmapowac na inty
-        data[i] = labelEncoder.fit_transform(data[i])
-    else:
-        columns_to_scale.append(i)
-columns_to_scale.remove("HeartDisease")
+data, encoder = get_data(encoder=chosen_encoder, force_reload=True)
 
-data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
-
-X = data.drop(['HeartDisease', 'RestingBP', 'RestingECG'], axis=1)
+X = data.drop(['HeartDisease'], axis=1)
 Y = data['HeartDisease']
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
@@ -36,4 +25,4 @@ metrics = classification_report(y_test, y_pred, output_dict=True)
 
 modelHandler = ModelHandler()
 
-modelHandler.add_model(model, "naive_bayes_prescaler", metrics)
+modelHandler.add_model(model, "naive_bayes", metrics, chosen_encoder)
